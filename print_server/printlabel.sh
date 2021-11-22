48e9fed38f3c0c1f2e04286602c3d388d1919397
m3hran <m3hran@gmail.com>
2021-11-22 18:06:18 -0500

    print wip

--- print_server/printlabel.sh
+++ print_server/printlabel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 glabels-3-batch -i labels.csv -o test.pdf template.glabels
-lp -d dymo -o media=w79h252 -o orientation-requested=4 -o cpi=13 -o page-left=12 -o page-top=12 test.pdf
+lp -d dymo -o media=w79h252 -o orientation-requested=4 -o cpi=13 -o page-left=12 -o page-top=12 test.pdf 
